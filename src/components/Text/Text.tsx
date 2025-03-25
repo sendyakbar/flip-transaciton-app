309ea142ed6b8c5f938a4f3829c8f800a5e382de
Sendy Akbar <msendyakbars@gmail.com>
2025-03-25 12:03:17 +0700

    chore(text): create reusable text component

---
+++ src/components/Text/Text.tsx
@@ -0,0 +1,45 @@
+import { forwardRef, ReactNode } from 'react';
+import {
+  Text as RNText,
+  TextProps as RNTextProps,
+  TextStyle,
+} from 'react-native';
+import { COLOR } from '../../theme/color';
+
+interface Props extends RNTextProps {
+  children: ReactNode;
+  size?: TextStyle['fontSize'];
+  weight?: TextStyle['fontWeight'];
+  color?: TextStyle['color'],
+  style?: TextStyle;
+}
+
+export const Text = forwardRef<RNText, Props>(
+  (props, ref) => {
+    const {
+      children,
+      size,
+      weight,
+      color,
+      style,
+      ...rest
+    } = props;
+
+    return (
+      <RNText
+        {...rest}
+        ref={ref}
+        style={[
+          {
+            fontSize: size || 14,
+            fontWeight: weight || '400',
+            color: color || COLOR.BLACK,
+          },
+          style,
+        ]}
+      >
+        {children}
+      </RNText>
+    );
+  }
+);
