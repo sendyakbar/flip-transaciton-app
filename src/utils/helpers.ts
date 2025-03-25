9658c56d4397515e654f1fd8396f1bd9ed3629e7
Sendy Akbar <msendyakbars@gmail.com>
2025-03-25 14:55:37 +0700

    chore(helpers): create format currency and format date helper function

---
+++ src/utils/helpers.ts
@@ -0,0 +1,38 @@
+export const formatCurrency = (amount: number) => (
+  `Rp${String(amount).replace(/\B(?=(\d{3})+(?!\d))/g, '.')}`
+);
+
+export const formatDate = (dateString: string) => {
+  const dateRegex = /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}$/;
+
+  if (!dateRegex.test(dateString)) {
+    throw new Error("Invalid date format. Expected format: 'YYYY-MM-DD HH:mm:ss'");
+  }
+
+  const date = new Date(dateString);
+
+  if (isNaN(date.getTime())) {
+    throw new Error('Invalid date value.');
+  }
+
+  const months = [
+    'Januari',
+    'Februari',
+    'Maret',
+    'April',
+    'Mei',
+    'Juni',
+    'Juli',
+    'Agustus',
+    'September',
+    'Oktober',
+    'November',
+    'Desember',
+  ];
+
+  const day = date.getDate();
+  const month = months[date.getMonth()];
+  const year = date.getFullYear();
+
+  return `${day} ${month} ${year}`;
+};
