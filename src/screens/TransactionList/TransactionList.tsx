5e423158bf5d791c856aaa11726f3591636c9763
Sendy Akbar <msendyakbars@gmail.com>
2025-03-25 14:43:47 +0700

    feat(transaction-list): transaction list done - wire data to card component - loading state implemented - pull to refresh implemented

--- src/screens/TransactionList/TransactionList.tsx
+++ src/screens/TransactionList/TransactionList.tsx
@@ -1,13 +1,57 @@
-import { FC } from 'react';
-import { View, Text } from 'react-native';
+import { FC, useCallback, useState } from 'react';
+import { ActivityIndicator, FlatList, View } from 'react-native';
 
 import { Props } from './types';
 import { styles } from './styles';
+import { useGetTransactions } from '../../services/query/transaction/useGetTransactions';
+import { TransactionCard } from '../../components/TransactionCard/TransactionCard';
+import { COLOR } from '../../theme/color';
 
 export const TransactionList: FC<Props> = () => {
+  const {
+    data,
+    isLoading,
+    refetch,
+  } = useGetTransactions({});
+  const [refreshing, setRefreshing] = useState(false);
+
+  const onRefresh = useCallback(async () => {
+    setRefreshing(true);
+    await refetch();
+    setRefreshing(false);
+  }, [refetch]);
+
+  const separator = useCallback(() => (
+    <View style={styles.separator} />
+  ), []);
+
+  if (isLoading) {
+    return (
+      <View style={styles.loading}>
+        <ActivityIndicator color={COLOR.SUCCESS} size="large" />
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Transaction List Screen</Text>
-    </View>
+    <FlatList
+      contentContainerStyle={styles.container}
+      data={data}
+      renderItem={({ item }) => (
+        <TransactionCard
+          beneficiaryBank={item.beneficiary_bank}
+          senderBank={item.sender_bank}
+          beneficiaryName={item.beneficiary_name}
+          amount={String(item.amount)}
+          date={item.completed_at}
+          status={item.status}
+          onPress={() => {}}
+        />
+      )}
+      keyExtractor={(_) => _.id}
+      ItemSeparatorComponent={separator}
+      onRefresh={onRefresh}
+      refreshing={refreshing}
+    />
   );
 };
--- src/screens/TransactionList/styles.ts
+++ src/screens/TransactionList/styles.ts
@@ -1,7 +1,20 @@
 import { StyleSheet } from 'react-native';
+import { COLOR } from '../../theme/color';
 
 export const styles = StyleSheet.create({
   container: {
+    flexGrow: 1,
+    backgroundColor: COLOR.BACKGROUND,
+    paddingHorizontal: 8,
+    paddingVertical: 16,
+  },
+  separator: {
+    height: 8,
+  },
+  loading: {
     flex: 1,
+    backgroundColor: COLOR.BACKGROUND,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
