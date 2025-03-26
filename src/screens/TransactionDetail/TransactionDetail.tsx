76852afbdad97b8dfaf5a2548baec537be7076ac
Sendy Akbar <msendyakbars@gmail.com>
2025-03-26 15:16:58 +0700

    feat(detail): create transaction detail screen

--- src/screens/TransactionDetail/TransactionDetail.tsx
+++ src/screens/TransactionDetail/TransactionDetail.tsx
@@ -1,12 +1,94 @@
-import { FC } from 'react';
-import { View, Text } from 'react-native';
+import { FC, useMemo } from 'react';
+import { Image, TouchableOpacity, View } from 'react-native';
 import { Props } from './types';
 import { styles } from './styles';
+import { Text } from '../../components/Text/Text';
+import { COLOR } from '../../theme/color';
+import { DetailSection } from '../../components/DetailSection/DetailSection';
+import { Destination } from '../../components/Destination/Destination';
+import { DetailItem } from '../../components/DetailItem/DetailItem';
+import { formatCurrency, formatDate } from '../../utils/helpers';
 
-export const TransactionDetail: FC<Props> = () => {
+export const TransactionDetail: FC<Props> = ({ navigation, route }) => {
+  const {
+    id,
+    sender_bank,
+    beneficiary_bank,
+    beneficiary_name,
+    account_number,
+    amount,
+    remark,
+    unique_code,
+    created_at,
+  } = route.params;
+
+  const data = useMemo(() => [
+    {
+      label: beneficiary_name,
+      value: account_number,
+    },
+    {
+      label: 'Nominal',
+      value: formatCurrency(amount),
+    },
+    {
+      label: 'Berita Transfer',
+      value: remark,
+    },
+    {
+      label: 'Kode Unik',
+      value: unique_code.toString(),
+    },
+    {
+      label: 'Waktu Dibuat',
+      value: formatDate(created_at),
+    },
+  ], [
+    account_number,
+    amount,
+    beneficiary_name,
+    created_at,
+    remark,
+    unique_code,
+  ]);
+
   return (
     <View style={styles.container}>
-      <Text>Transaction Detail Screen</Text>
+      <View style={styles.wrapper}>
+        <DetailSection>
+          <View style={styles.row}>
+            <Text size={18} weight="bold">ID TRANSAKSI: #{id}</Text>
+            <TouchableOpacity activeOpacity={0.7}>
+              <Image
+                source={require('../../assets/icons/icon_copy.jpeg')}
+                style={styles.icon}
+                resizeMode="contain"
+              />
+            </TouchableOpacity>
+          </View>
+        </DetailSection>
+        <DetailSection>
+          <View style={styles.rowBetween}>
+            <Text size={18} weight="bold">DETAIL TRANSAKSI</Text>
+            <TouchableOpacity
+              activeOpacity={0.7}
+              onPress={() => {
+                navigation.goBack();
+              }}
+            >
+              <Text size={18} color={COLOR.WARNING} weight="bold">Tutup</Text>
+            </TouchableOpacity>
+          </View>
+        </DetailSection>
+        <DetailSection>
+          <Destination sender={sender_bank} destination={beneficiary_bank} />
+          <View style={styles.detail}>
+            {data.map((item, index) => (
+              <DetailItem {...item} key={index} />
+            ))}
+          </View>
+        </DetailSection>
+      </View>
     </View>
   );
 };
--- src/screens/TransactionDetail/styles.ts
+++ src/screens/TransactionDetail/styles.ts
@@ -1,7 +1,36 @@
 import { StyleSheet } from 'react-native';
+import { COLOR } from '../../theme/color';
 
 export const styles = StyleSheet.create({
+  wrapper: {
+    gap: 2,
+  },
   container: {
     flex: 1,
+    backgroundColor: COLOR.BACKGROUND,
+    paddingTop: 20,
+  },
+  section: {
+    backgroundColor: COLOR.WHITE,
+    padding: 20,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  icon: {
+    height: 20,
+    width: 20,
+  },
+  rowBetween: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  detail: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 16,
   },
 });
